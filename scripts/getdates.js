4c64989144d3b6de14e605efa0db60aae3355946
GustavoFragasAlest <gustavo.fragas@alest.com.br>
2026-03-07 00:30:16 -0300

    finished, fixing some errors

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,13 +1,6 @@
 const currentyear = document.querySelector("#current-year");
 const lastupdated = document.querySelector("#last-updated");
 
-const date = new Date();
-const year = date.getFullYear();
-const day = date.getDate();
-const month = date.getMonth() + 1;
-const hours = date.getHours();
-const minutes = date.getMinutes();
-
-currentyear.textContent = year;
-lastupdated.textContent = `Last Updated: ${month}/${day}/${year} ${hours}:${minutes}`;
+currentyear.textContent = new Date().getFullYear();
+lastupdated.textContent = document.lastModified;
 
